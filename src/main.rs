e7d3abfba975f0bdcb0cdec687c4793b6071fdcf
Fenhl <fenhl@fenhl.net>
2021-03-08 08:03:33 +0000

    Update async-proto dependency

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,14 @@ use {
         time::Duration,
     },
     derive_more::From,
-    futures::{
-        SinkExt as _,
-        stream::{
-            SplitSink,
-            Stream,
-            StreamExt as _,
-            TryStreamExt as _,
-        },
+    futures::stream::{
+        SplitSink,
+        Stream,
+        StreamExt as _,
     },
     async_proto::{
         Protocol,
-        impls::StringReadError,
+        ReadError,
     },
     gefolge_web::login::Mensch,
     pyo3::PyErr,
@@ -44,8 +40,8 @@ type WsSink = Arc<Mutex<SplitSink<WebSocket, Message>>>;
 pub enum Error {
     EndOfStream,
     Python(PyErr),
+    Read(ReadError),
     RicochetRobots(ricochet_robots_websocket::Error),
-    StringRead(StringReadError),
     UnknownApiKey,
     Warp(warp::Error),
 }
@@ -55,8 +51,8 @@ impl fmt::Display for Error {
         match self {
             Error::EndOfStream => write!(f, "reached end of stream"),
             Error::Python(e) => write!(f, "Python error: {}", e),
+            Error::Read(e) => e.fmt(f),
             Error::RicochetRobots(e) => write!(f, "error in Ricochet Robots session: {}", e),
-            Error::StringRead(e) => write!(f, "{:?}", e),
             Error::UnknownApiKey => write!(f, "unknown API key"),
             Error::Warp(e) => e.fmt(f),
         }
@@ -81,17 +77,14 @@ impl ServerMessage {
     }
 }
 
-pub async fn client_session(mut stream: impl Stream<Item = Result<Message, warp::Error>> + Unpin, sink: WsSink) -> Result<(), Error> {
-    let packet = stream.try_next().await?.ok_or(Error::EndOfStream)?;
-    let api_key = String::read(packet.as_bytes()).await?;
+pub async fn client_session(mut stream: impl Stream<Item = Result<Message, warp::Error>> + Unpin + Send, sink: WsSink) -> Result<(), Error> {
+    let api_key = String::read_ws(&mut stream).await?;
     let user = Mensch::by_api_key(&api_key)?.ok_or(Error::UnknownApiKey)?; // verify API key
     let ping_sink = Arc::clone(&sink);
     tokio::spawn(async move {
         loop {
             sleep(Duration::from_secs(30)).await;
-            let mut buf = Vec::default();
-            if ServerMessage::Ping.write(&mut buf).await.is_err() { break } //TODO better error handling
-            if ping_sink.lock().await.send(Message::binary(buf)).await.is_err() { break } //TODO better error handling
+            if ServerMessage::Ping.write_ws(&mut *ping_sink.lock().await).await.is_err() { break } //TODO better error handling
         }
     });
     ricochet_robots_websocket::client_session(user, stream, sink).await?;
@@ -102,11 +95,7 @@ async fn client_connection(ws: WebSocket) {
     let (ws_sink, ws_stream) = ws.split();
     let ws_sink = Arc::new(Mutex::new(ws_sink));
     if let Err(e) = client_session(ws_stream, Arc::clone(&ws_sink)).await {
-        let message = ServerMessage::from_error(e);
-        let mut buf = Vec::default();
-        if message.write(&mut buf).await.is_ok() {
-            let _ = ws_sink.lock().await.send(Message::binary(buf)).await;
-        }
+        let _ = ServerMessage::from_error(e).write_ws(&mut *ws_sink.lock().await).await;
     }
 }
 
