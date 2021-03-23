88ef88ad360ca33c6818ff2b899c25ed4ae1091f
Fenhl <fenhl@fenhl.net>
2021-03-23 06:31:28 +0000

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -83,16 +83,16 @@ enum SessionPurpose {
 }
 
 pub async fn client_session(mut stream: impl Stream<Item = Result<Message, warp::Error>> + Unpin + Send, sink: WsSink) -> Result<(), Error> {
-    let api_key = String::read_ws(&mut stream).await?;
+    let api_key = String::read_warp(&mut stream).await?;
     let user = Mensch::by_api_key(&api_key)?.ok_or(Error::UnknownApiKey)?; // verify API key
     let ping_sink = Arc::clone(&sink);
     tokio::spawn(async move {
         loop {
             sleep(Duration::from_secs(30)).await;
-            if ServerMessage::Ping.write_ws(&mut *ping_sink.lock().await).await.is_err() { break } //TODO better error handling
+            if ServerMessage::Ping.write_warp(&mut *ping_sink.lock().await).await.is_err() { break } //TODO better error handling
         }
     });
-    match SessionPurpose::read_ws(&mut stream).await? {
+    match SessionPurpose::read_warp(&mut stream).await? {
         SessionPurpose::RicochetRobots => ricochet_robots_websocket::client_session(user, stream, sink).await?,
     }
     Ok(())
@@ -102,7 +102,7 @@ async fn client_connection(ws: WebSocket) {
     let (ws_sink, ws_stream) = ws.split();
     let ws_sink = Arc::new(Mutex::new(ws_sink));
     if let Err(e) = client_session(ws_stream, Arc::clone(&ws_sink)).await {
-        let _ = ServerMessage::from_error(e).write_ws(&mut *ws_sink.lock().await).await;
+        let _ = ServerMessage::from_error(e).write_warp(&mut *ws_sink.lock().await).await;
     }
 }
 
